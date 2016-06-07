ceb82f01a6833f6a8f6b06dbb4e6af16479205a1
Kendra Lyndon <kendralyndon@gmail.com>
2016-06-07 12:03:25 -0600

    convert vanilla js to jquery

--- main.js
+++ main.js
@@ -1,21 +1,13 @@
-document.addEventListener('DOMContentLoaded',function(){
-  var request = new XMLHttpRequest();
-  var covers = document.getElementById('covers');
-  request.onreadystatechange = function(){
-    if(request.readyState === 4){
-      if(request.status < 400){
-        var results = JSON.parse(request.responseText).results;
-        var random = makeRandomArray();
-        for(var i=0;i<3;i++){
-          var coverArt = document.createElement('img');
-          coverArt.className = 'album-img';
-          coverArt.id = 'album-'+i;
-          coverArt.src = 'images/'+results[random[i]].cover_art;
-          covers.appendChild(coverArt);
-        }
-      }
+$(document).ready(function(){
+  var covers = $('#covers');
+  $.getJSON('https://lit-fortress-6467.herokuapp.com/object', function(data){
+    var results = data.results;
+    for(var i=0;i<3;i++){
+      var coverArt = $('<img>');
+      coverArt.attr('src', 'images/'+results[makeRandomArray()[i]].cover_art);
+      coverArt.addClass('album-img');
+      coverArt.attr('id','album-'+i);
+      coverArt.appendTo(covers);
     }
-  }
-  request.open('GET','https://lit-fortress-6467.herokuapp.com/object');
-  request.send();
+  });
 })
--- playlist.js
+++ playlist.js
@@ -1,59 +1,35 @@
-document.addEventListener('DOMContentLoaded',function(){
-  var covers = document.getElementById('covers');
-  var entry = document.getElementById('tracks');
-  var clear = document.getElementById('clear');
-  var submit = document.getElementById('submit');
-  var request = new XMLHttpRequest();
-  var postRequest = new XMLHttpRequest();
-
-  request.onreadystatechange = function(){
-    if(request.readyState === 4){
-      if(request.status < 400){
-        var results = JSON.parse(request.responseText).results;
-        for(var i=0;i<results.length;i++){
-          var cover = document.createElement('div');
-          cover.className = 'album';
-          cover.id = results[i].id;
-          var image = document.createElement('img');
-          image.className = 'cover_img';
-          image.src = 'images/'+results[i].cover_art;
-          cover.appendChild(image);
-          covers.appendChild(cover);
-        }
-
-        var album = document.getElementsByClassName('album');
-
-        for(var i=0;i<album.length;i++){
-          album[i].addEventListener('click',function(){
-            for(var j=0;j<results.length;j++){
-              if(results[j].id === Number(this.id)){
-                var artist = results[j].artist;
-                var title = results[j].title;
-                entry.textContent+=`${artist}: ${title}\n`;
-              }
-            }
-          })
+$(document).ready(function(){
+  var covers = $('#covers');
+  var tracks = $('#tracks');
+  var clear = $('#clear');
+  var submit = $('#submit');
+  $.getJSON('https://lit-fortress-6467.herokuapp.com/object',function(data){
+    var results = data.results;
+    for(var i=0;i<results.length;i++){
+      var cover = $('<div>');
+      cover.addClass('album');
+      cover.attr('id',results[i].id);
+      var image = $('<img>');
+      image.addClass('cover_img');
+      image.attr('src', 'images/'+results[i].cover_art);
+      image.appendTo(cover);
+      cover.appendTo(covers);
+    }
+    var album = $('.album');
+    album.on('click',function(){
+      for(var j=0;j<results.length;j++){
+        if(results[j].id === Number(this.id)){
+          tracks.append(`${results[j].artist}: ${results[j].title}\n`);
         }
       }
-    }
-  }
-  request.open('GET','https://lit-fortress-6467.herokuapp.com/object');
-  request.send();
-
-  clear.addEventListener('click',function(){
-    tracks.textContent = '';
+    })
   })
-
-  submit.addEventListener('click',function(){
-    postRequest.onreadystatechange = function(){
-      if(postRequest.readyState === 4){
-        if(postRequest.status < 400){
-          console.log(postRequest.responseText);
-        }
-      }
-    }
-    postRequest.open('POST','https://lit-fortress-6467.herokuapp.com/post');
-    postRequest.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    postRequest.send();
+  clear.click(function(){
+    tracks.text('');
+  })
+  submit.click(function(){
+    $.post('https://lit-fortress-6467.herokuapp.com/post',function(response){
+      console.log(response);
+    })
   })
 })
